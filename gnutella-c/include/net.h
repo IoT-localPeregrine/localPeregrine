dd753c6e9ea7927f6bb06629b3440fce31b2ae3d
dmittrey <dmitryzubakhin@mail.ru>
2023-06-20 17:14:13 +0300

    Add self ref in subscribe methods

--- gnutella-c/include/net.h
+++ gnutella-c/include/net.h
@@ -18,7 +18,7 @@ NetError inet_pong(struct INet, Address targ_adr, Peer peer);
 NetError inet_query(struct INet, String criteria);
 NetError inet_query_hit(struct INet, QueryHit query_hit);
 
-NetError inet_subscribe_ping(void (*consumer_fnc)());
-NetError inet_subscribe_pong(void (*consumer_fnc)(Peer peer));
-NetError inet_subscribe_query(void (*consumer_fnc)(struct string criteria));
-NetError inet_subscribe_query_hit(void (*consumer_fnc)(QueryHit hit_details));
+NetError inet_subscribe_ping(struct INet, void (*consumer_fnc)());
+NetError inet_subscribe_pong(struct INet, void (*consumer_fnc)(Peer peer));
+NetError inet_subscribe_query(struct INet, void (*consumer_fnc)(struct string criteria));
+NetError inet_subscribe_query_hit(struct INet, void (*consumer_fnc)(QueryHit hit_details));
