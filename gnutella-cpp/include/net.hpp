0a38760cf2de029a6389acf46d9bfe4de127bca8
dmittrey <dmitryzubakhin@mail.ru>
2023-06-21 05:11:13 +0300

    Done cpp network layer interface

--- gnutella-cpp/include/net.hpp
+++ gnutella-cpp/include/net.hpp
@@ -5,27 +5,45 @@
 
 #include "query_hit.hpp"
 #include "peer.hpp"
+#include "utils.hpp"
 
 #include "../gnutella-c/include/error.h"
+#include "../gnutella-c/include/net.h"
 
 namespace gnutella_cpp
 {
-    namespace INet
+    namespace inet
     {
         void init(std::string);
         void exit();
 
         NetError connect_to_network(std::string network_name);
-        NetError explore_networks(std::function<void(std::string)> explr_consumer_fnc);
+        NetError explore_networks(void (*)(SString));
 
         NetError create(std::string network_name);
 
         NetError ping();
         NetError query(std::string criteria);
 
-        NetError subscribe_ping(std::function<Peer()> ping_consumer_fnc);
-        NetError subscribe_pong(std::function<void(Peer)> ping_consumer_fnc);
-        NetError subscribe_query(std::function<QueryHit(std::string)> query_consumer_fnc);
-        NetError subscribe_query_hit(std::function<void(QueryHit)> query_hit_consumer_fnc);
+        NetError subscribe_ping(Peer (*)());
+        NetError subscribe_pong(void (*)(Peer));
+        NetError subscribe_query(QueryHit (*)(SString));
+        NetError subscribe_query_hit(void (*)(QueryHit));
     };
 }
+
+void gnutella_cpp::inet::init(std::string net_name) { inet_init(utils::ToSString(net_name)); }
+void gnutella_cpp::inet::exit() { inet_exit(); }
+
+NetError gnutella_cpp::inet::connect_to_network(std::string network_name) { inet_connect_to_network(utils::ToSString(network_name)); }
+NetError gnutella_cpp::inet::explore_networks(void (*callback)(SString)) { inet_explore_networks((Wrapper_ExploreNetworkConsumerFunc){callback}); }
+
+NetError gnutella_cpp::inet::create(std::string net_name) { inet_create(utils::ToSString(net_name)); }
+
+NetError gnutella_cpp::inet::ping() { inet_ping(); }
+NetError gnutella_cpp::inet::query(std::string criteria) { inet_query(utils::ToSString(criteria)); }
+
+NetError subscribe_ping(Peer (*callback)()) { inet_subscribe_ping((Wrapper_PingConsumerFunc){callback}); }
+NetError subscribe_pong(void (*callback)(Peer)) { inet_subscribe_pong((Wrapper_PongConsumerFunc){callback}); }
+NetError subscribe_query(QueryHit (*callback)(SString)) { inet_subscribe_query((Wrapper_QueryConsumerFunc){callback}); }
+NetError subscribe_query_hit(void (*callback)(QueryHit)) { inet_subscribe_query_hit((Wrapper_QueryHitConsumerFunc){callback}); }
--- gnutella-cpp/src/net.c
+++
@@ -1,3 +0,0 @@
-#include "./gnutella-cpp/include/net.hpp"
-
-void gnutella_cpp::
